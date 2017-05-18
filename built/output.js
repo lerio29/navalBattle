679c38afac9c460c3b859448932fa2c3a30231c6
chicha29 <chicha29@free.fr>
2017-05-18 16:27:54 +0200

    debut migration typescript

--- built/output.js
+++
@@ -1,9 +0,0 @@
-cexport;
-var MatriceCase = (function () {
-    function MatriceCase(hor, vert, status) {
-        this.hor = hor;
-        this.vert = vert;
-        this.state = state;
-    }
-    return MatriceCase;
-}());
