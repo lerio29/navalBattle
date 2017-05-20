0bad64835378516489ca572cd1f764b4456291eb
lerio29 <chicha29@free.fr>
2017-05-20 02:18:59 +0200

    tyescript fonctionnel

--- built/enumStatus.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var EnumStatus;
-(function (EnumStatus) {
-    EnumStatus[EnumStatus["STATUS_EMPTY"] = 0] = "STATUS_EMPTY";
-    EnumStatus[EnumStatus["STATUS_HIT"] = 1] = "STATUS_HIT";
-    EnumStatus[EnumStatus["STATUS_FAIL"] = 2] = "STATUS_FAIL";
-})(EnumStatus = exports.EnumStatus || (exports.EnumStatus = {}));
