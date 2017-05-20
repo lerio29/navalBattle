671128381f04ae2bf4b067d9327e9da1b6fcc584
lerio29 <chicha29@free.fr>
2017-05-20 02:45:37 +0200

    browserify

---
+++ built/enumStatus.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var EnumStatus;
+(function (EnumStatus) {
+    EnumStatus[EnumStatus["STATUS_EMPTY"] = 0] = "STATUS_EMPTY";
+    EnumStatus[EnumStatus["STATUS_HIT"] = 1] = "STATUS_HIT";
+    EnumStatus[EnumStatus["STATUS_FAIL"] = 2] = "STATUS_FAIL";
+})(EnumStatus = exports.EnumStatus || (exports.EnumStatus = {}));
---
+++ built/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var BattleMatrice = require("./battleMatrice");
+console.log("dsknkdsk");
+var grille = new BattleMatrice.BattleMatrice(12);
