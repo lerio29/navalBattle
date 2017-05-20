e01da6c2fede86df647451b93194c1bd0839bc36
lerio29 <chicha29@free.fr>
2017-05-20 02:46:46 +0200

    browserify

--- built/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var BattleMatrice = require("./battleMatrice");
-console.log("dsknkdsk");
-var grille = new BattleMatrice.BattleMatrice(12);
