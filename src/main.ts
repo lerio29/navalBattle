976ea14f6bb05cf07bd808415c641bc494692491
chicha29 <chicha29@free.fr>
2017-05-29 12:21:16 +0200

    ajout jquery fonctionnel

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import * as BattleMatrice from "./battleMatrice"; 
-import * as Jsdom from "jsdom";
+// import * as Jsdom from "jsdom";
+import * as $ from "jquery";
 
 
 
 let grille: BattleMatrice.BattleMatrice = new BattleMatrice.BattleMatrice(12);
 
-let documentFragment:any = Jsdom.jsdom();
+// let documentFragment:any = Jsdom.jsdom();
 
-let elem:any = documentFragment.querySelector("#grid");
+// let elem:any = documentFragment.querySelector(".grid");
 
-console.log(elem);
+console.log($(".grid").attr("toto-data"));
+
+// console.log("eleme : "+ elem);
+
