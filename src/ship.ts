2479d6f915e2e5e802d0f6f9196f974bb1d41c31
lerio29 <chicha29@free.fr>
2017-10-06 17:25:33 +0200

    debut de la classe ship.ts et methode update

---
+++ src/ship.ts
@@ -0,0 +1,14 @@
+import * as EnumShip from "./enumShip";
+import * as MatriceCase from "./matriceCase";
+import * as Collections from 'typescript-collections';
+
+export class Ship{
+
+	private _shipType: EnumShip.EnumShip;
+	private _shipPosition: Collections.Set<MatriceCase.MatriceCase>;
+
+	constructor(){
+
+	}
+
+}
