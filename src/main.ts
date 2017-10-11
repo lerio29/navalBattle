df0854611102fa15c07b63b601a198a81f4089be
lerio29 <chicha29@free.fr>
2017-10-11 10:12:19 +0200

    creation implemntation porteAvion

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 
 import * as Collections from 'typescript-collections';
 import * as BattleMatrice from "./battleMatrice"; 
+import * as MatriceCase from "./matriceCase"; 
+import * as Ship from "./ship";
+import * as Pa from "./porteAvion";
 
 //ajout des @types pour TypeScript
 import * as Jsdom from "jsdom";
@@ -8,10 +11,17 @@ import * as Angular from "angular";
 import * as $ from "jquery";
 
 
+
+
 let grillePlayer1: BattleMatrice.BattleMatrice = new BattleMatrice.BattleMatrice(10);
-// grillePlayer1.addShip(new Ship());
+// let player1pa: Ship.Ship = new Pa.PorteAvion();
+
+// grillePlayer1.addShip(new Pa.PorteAvion(new Collections.Dictionary<string,MatriceCase.MatriceCase>()));
 
 // let grillePlayer2: BattleMatrice.BattleMatrice = new BattleMatrice.BattleMatrice(10);
+
+
+
 
 // let documentFragment:any = Jsdom.jsdom();
 
@@ -21,7 +31,7 @@ let grillePlayer1: BattleMatrice.BattleMatrice = new BattleMatrice.BattleMatrice
 
 //TODO
 for(let item in grillePlayer1.getMatrice.values) {
-	console.log("item : " + item.);
+	console.log("item : " + item.toString);
 }
 
 
