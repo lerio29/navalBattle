e10e1b7a840988816f115774f91673b0eff7df2f
lerio29 <chicha29@free.fr>
2017-10-11 09:18:42 +0200

    creation abstract method ship

--- src/ship.ts
+++ src/ship.ts
@@ -2,16 +2,80 @@
 import * as Collections from 'typescript-collections';
 import * as MatriceCase from "./matriceCase"; 
 import * as EnumShip from "./enumShip";
+import * as EnumStatus from "./enumStatus";
 
 
 
-	export class Ship{
+	export abstract class Ship {
 
-		private _shipType: EnumShip.EnumShip;
-		private _shipPosition: Collections.Set<MatriceCase.MatriceCase>;
+		protected _shipName: string;
+		protected _shipType: EnumShip.EnumShip;
+		protected _shipPosition: Collections.Dictionary<string,MatriceCase.MatriceCase>;
+		protected _shipSize: number;
+		protected _startCase: MatriceCase.MatriceCase;
+		protected _endCase: MatriceCase.MatriceCase;
 
-		constructor(){
 
+		/**
+		 * [constructor description]
+		 * @param {string}                                                 shipName     [description]
+		 * @param {EnumShip.EnumShip}                                      shipType     [description]
+		 * @param {number}                                                 shipSize     [description]
+		 * @param {Collections.Dictionary<string,MatriceCase.MatriceCase>} shipPosition [description]
+		 * @param {MatriceCase.MatriceCase}                                startCase    [description]
+		 * @param {MatriceCase.MatriceCase}                                endCase      [description]
+		 */
+		constructor(shipName: string, shipType: EnumShip.EnumShip, shipSize: number, 
+			shipPosition: Collections.Dictionary<string,MatriceCase.MatriceCase>,
+			startCase?: MatriceCase.MatriceCase, endCase?: MatriceCase.MatriceCase){
+			
+			this._shipName = shipName;
+			this._shipType = shipType;
+			this._shipSize = shipSize;
+			this._shipPosition = shipPosition;
+
+			//TODO optionnelle puisque peuvent être deduit a partir du shipPosition 			
+			this._startCase = startCase;
+			this._endCase = endCase;
 		}
+
+		abstract calStartCase(): void;
+
+		abstract calEndCase(): void;
+		
+
+		get getShipName() :string{
+			return this._shipName;
+		}
+
+		set setShipName(name :string){
+			this._shipName = name;
+		}
+
+		get getShipType() :EnumShip.EnumShip{
+			return this._shipType;
+		}
+
+		set setShipType(type :EnumShip.EnumShip){
+			this._shipType = type;
+		}
+
+		get getShipSize() :number{
+			return this._shipSize;
+		}
+
+		set setShipSize(size :number){
+			this._shipSize = size;
+		}
+
+		get getPosition() :Collections.Dictionary<string,MatriceCase.MatriceCase>{
+			return this._shipPosition;
+		}
+
+		set Position(position :Collections.Dictionary<string,MatriceCase.MatriceCase>){
+			this._shipPosition = position;
+		}
+
+
 
 	}
